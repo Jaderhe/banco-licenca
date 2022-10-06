d5fca3f0a01e5e83b84fb4ab9feffe2d9524e67b
jaderhe <jaderhericks@gmail.com>
2022-10-06 13:31:14 -0300

    criado base do projeto com spring initializer

---
+++ API/src/test/kotlin/br/gov/pr/toledo/API/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.gov.pr.toledo.API
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
