bc10a8c39f7e0b1e56ac89a94878820c34010c10
jaderhe <jaderhericks@gmail.com>
2022-10-21 16:14:08 -0300

    iniciado mapeamento da classe software, falta fk com user

--- API/src/main/kotlin/br/gov/pr/toledo/api/domain/software/model/IdSoftware.kt
+++ API/src/main/kotlin/br/gov/pr/toledo/api/domain/software/model/IdSoftware.kt
@@ -1,4 +1,9 @@
 package br.gov.pr.toledo.api.domain.software.model
 
-class IdSoftware {
-}
+import com.fasterxml.jackson.annotation.JsonValue
+import java.io.Serializable
+import java.util.*
+import javax.persistence.Embeddable
+
+@Embeddable
+data class IdSoftware(@field:JsonValue var id: UUID = UUID.randomUUID()) : Serializable
--- API/src/main/kotlin/br/gov/pr/toledo/api/domain/software/model/Software.kt
+++ API/src/main/kotlin/br/gov/pr/toledo/api/domain/software/model/Software.kt
@@ -1,4 +1,27 @@
 package br.gov.pr.toledo.api.domain.software.model
 
-class Software {
+import br.gov.pr.toledo.api.domain.users.model.User
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "software")
+class Software (
+    @EmbeddedId
+    @Column(name = "software_id")
+    val softwareId: IdSoftware,
+    @Column(name = "name")
+    val name: String,
+    @Column(name = "created_at")
+    val createdAt: LocalDate,
+    @Column(name = "updated_at")
+    val updatedAt: LocalDate,
+
+    /*
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "ID")
+    private Teacher teacher;*/
+
+    // finalizar relacionamento com user
+    ) {
 }
